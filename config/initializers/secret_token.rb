8f1d418a2c0e3660fba7c951ced49ab2867b4539
blattus <roshan.sadanani+github@gmail.com>
2013-02-11 21:10:14 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PreptimeLandingpage::Application.config.secret_token = '881e3cd33ced3e155200658fbee0c8a4ba7fd9c9b49a859c41e3cdff9c837f2cff413d0200c4b2da2d0c318521212c9de440a95c966332c99d1e0e44b6978ccf'
